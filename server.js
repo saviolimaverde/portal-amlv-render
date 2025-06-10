049dbaca9de4563f38b4faf8ff7a4afe5064b8a7
Sávio Lima Verde <savio@amlvadvocacia.com.br>
2025-06-10 20:09:47 -0300

    Adicionado redirecionamento para domínio oficial

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// 🔁 Redireciona automaticamente do domínio padrão da Render para o domínio personalizado
+app.use((req, res, next) => {
+  if (req.headers.host === 'portal-amlv-render.onrender.com') {
+    return res.redirect(301, 'https://portal.amlvadvocacia.com.br' + req.url);
+  }
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
